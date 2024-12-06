6002bafaa31793acd8df227569179a864e312f95
Rémi Marseault <larsluph@larsluph.dev>
2024-12-06 02:52:54 +0100

    Clavier infernal (#5)
    Co-authored-by: Lucas Dumont <lucas.dumont1@viacesi.fr>

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -7,6 +7,11 @@ const routes = [
     component: () => import('@/components/Human/Human.vue')
   },
   {
+    name: 'input',
+    path: '/input',
+    component: () => import('@/components/FormRelou.vue')
+  },
+  {
     name: 'organ',
     path: '/:organ',
     component: () => import('@/components/Human/OrganDetail.vue')
